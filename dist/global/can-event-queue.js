2e08f370870dd0d86725e8cfc385b466ca298a52
Justin Meyer <justinbmeyer@gmail.com>
2017-10-05 15:38:17 -0500

    0.1.1

---
+++ dist/global/can-event-queue.js
@@ -0,0 +1,282 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval) {
+	// jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name) {
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for (i = 0; i < parts.length; i++) {
+			if (!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val) {
+		var parts = name.split("."),
+			cur = global,
+			i,
+			part,
+			next;
+		for (i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if (!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod) {
+		if (!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, default: true };
+		for (var p in mod) {
+			if (!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" && deps[1] === "exports" && deps[2] === "module"
+		);
+	};
+
+	var modules =
+		(global.define && global.define.modules) ||
+		(global._define && global._define.modules) ||
+		{};
+	var ourDefine = (global.define = function(moduleName, deps, callback) {
+		var module;
+		if (typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for (i = 0; i < deps.length; i++) {
+			args.push(
+				exports[deps[i]]
+					? get(exports[deps[i]])
+					: modules[deps[i]] || get(deps[i])
+			);
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		} else if (!args[0] && deps[0] === "exports") {
+			// Babel uses the exports and module object.
+			module = { exports: {} };
+			args[0] = module.exports;
+			if (deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if (!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if (globalExport && !get(globalExport)) {
+			if (useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	});
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function() {
+		// shim for @@global-helpers
+		var noop = function() {};
+		return {
+			get: function() {
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load) {
+				doEval(__load.source, global);
+			}
+		};
+	});
+})(
+	{},
+	typeof self == "object" && self.Object == Object ? self : window,
+	function(__$source__, __$global__) {
+		// jshint ignore:line
+		eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+	}
+);
+
+/*can-event-queue@0.1.0#can-event-queue*/
+define('can-event-queue', [
+    'require',
+    'exports',
+    'module',
+    'can-util/js/dev/dev',
+    'can-util/js/assign/assign',
+    'can-queues',
+    'can-reflect',
+    'can-symbol',
+    'can-key-tree'
+], function (require, exports, module) {
+    var canDev = require('can-util/js/dev/dev');
+    var assign = require('can-util/js/assign/assign');
+    var queues = require('can-queues');
+    var canReflect = require('can-reflect');
+    var canSymbol = require('can-symbol');
+    var KeyTree = require('can-key-tree');
+    var metaSymbol = canSymbol.for('can.meta');
+    function getHandlers(obj) {
+        var meta = obj[metaSymbol];
+        if (!meta) {
+            canReflect.setKeyValue(obj, metaSymbol, meta = {});
+        }
+        var handlers = meta.handlers;
+        if (!handlers) {
+            handlers = meta.handlers = new KeyTree([
+                Object,
+                Object,
+                Object,
+                Array
+            ], {
+                onFirst: function () {
+                    if (obj._eventSetup) {
+                        obj._eventSetup();
+                    }
+                },
+                onEmpty: function () {
+                    if (obj._eventTeardown) {
+                        obj._eventTeardown();
+                    }
+                }
+            });
+        }
+        return handlers;
+    }
+    var props = {
+        dispatch: function (event, args) {
+            if (!this.__inSetup) {
+                if (typeof event === 'string') {
+                    event = { type: event };
+                }
+                var handlers = getHandlers(this);
+                var handlersByType = handlers.getNode([event.type]);
+                if (handlersByType) {
+                    queues.batch.start();
+                    if (handlersByType.onKeyValue) {
+                        queues.enqueueByQueue(handlersByType.onKeyValue, this, args, function () {
+                            return {};
+                        });
+                    }
+                    if (handlersByType.event) {
+                        event.batchNum = queues.batch.number();
+                        var eventAndArgs = [event].concat(args);
+                        queues.enqueueByQueue(handlersByType.event, this, eventAndArgs, function () {
+                            return {};
+                        });
+                    }
+                    queues.batch.stop();
+                }
+            }
+        },
+        addEventListener: function (key, handler, queueName) {
+            getHandlers(this).add([
+                key,
+                'event',
+                queueName || 'mutate',
+                handler
+            ]);
+        },
+        removeEventListener: function (key, handler, queueName) {
+            getHandlers(this).delete([
+                key,
+                'event',
+                queueName || 'mutate',
+                handler
+            ]);
+        }
+    };
+    props.on = props.addEventListener;
+    props.off = props.removeEventListener;
+    var symbols = {
+        'can.onKeyValue': function (key, handler, queueName) {
+            getHandlers(this).add([
+                key,
+                'onKeyValue',
+                queueName || 'mutate',
+                handler
+            ]);
+        },
+        'can.offKeyValue': function (key, handler, queueName) {
+            getHandlers(this).delete([
+                key,
+                'onKeyValue',
+                queueName || 'mutate',
+                handler
+            ]);
+        }
+    };
+    var eventQueue = function (obj) {
+        assign(obj, props);
+        canReflect.assignSymbols(obj, symbols);
+        return obj;
+    };
+    function defineNonEnumerable(obj, prop, value) {
+        Object.defineProperty(obj, prop, {
+            enumerable: false,
+            value: value
+        });
+    }
+    assign(eventQueue, props);
+    defineNonEnumerable(eventQueue, 'start', function () {
+        console.warn('use can-queues.batch.start()');
+        queues.batch.start();
+    });
+    defineNonEnumerable(eventQueue, 'stop', function () {
+        console.warn('use can-queues.batch.stop()');
+        queues.batch.stop();
+    });
+    defineNonEnumerable(eventQueue, 'flush', function () {
+        console.warn('use can-queues.flush()');
+        queues.flush();
+    });
+    defineNonEnumerable(eventQueue, 'afterPreviousEvents', function (handler) {
+        console.warn('don\'t use afterPreviousEvents');
+        queues.mutateQueue.enqueue(function afterPreviousEvents() {
+            queues.mutateQueue.enqueue(handler);
+        });
+        queues.flush();
+    });
+    defineNonEnumerable(eventQueue, 'after', function (handler) {
+        console.warn('don\'t use after');
+        queues.mutateQueue.enqueue(handler);
+        queues.flush();
+    });
+    module.exports = eventQueue;
+});
+/*[global-shim-end]*/
+(function(global) { // jshint ignore:line
+	global._define = global.define;
+	global.define = global.define.orig;
+}
+)(typeof self == "object" && self.Object == Object ? self : window);
