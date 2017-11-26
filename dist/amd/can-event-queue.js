64f8c2b3c42074d2c880a33b367c7520cb8897e7
Justin Meyer <justinbmeyer@gmail.com>
2017-11-26 14:52:06 -0600

    0.12.1

---
+++ dist/amd/can-event-queue.js
@@ -0,0 +1,10 @@
+/*can-event-queue@0.12.0#can-event-queue*/
+define([
+    'require',
+    'exports',
+    'module',
+    './map/legacy/legacy'
+], function (require, exports, module) {
+    console.warn('import can-event-queue/map/legacy/legacy instead.');
+    module.exports = require('./map/legacy/legacy');
+});
