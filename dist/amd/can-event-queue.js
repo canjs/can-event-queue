d10f60724bd93c807246d21073cdeb56a322d97a
Kevin Phillips <kevin@bitovi.com>
2017-12-15 15:57:53 -0600

    0.15.2

---
+++ dist/amd/can-event-queue.js
@@ -0,0 +1,10 @@
+/*can-event-queue@0.15.1#can-event-queue*/
+define([
+    'require',
+    'exports',
+    'module',
+    './map/map'
+], function (require, exports, module) {
+    console.warn('import can-event-queue/map/map instead.');
+    module.exports = require('./map/map');
+});
