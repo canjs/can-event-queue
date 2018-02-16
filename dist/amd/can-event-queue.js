0eefde95aaf029d1e9b17c6df4718b90e8afbcb9
Christopher Baker <christopher@bitovi.com>
2018-02-15 16:30:59 -0800

    1.0.1

---
+++ dist/amd/can-event-queue.js
@@ -0,0 +1,10 @@
+/*can-event-queue@1.0.0#can-event-queue*/
+define([
+    'require',
+    'exports',
+    'module',
+    './map/map'
+], function (require, exports, module) {
+    console.warn('import can-event-queue/map/map instead.');
+    module.exports = require('./map/map');
+});
---
+++ dist/cjs/can-event-queue.js
@@ -0,0 +1,3 @@
+/*can-event-queue@1.0.0#can-event-queue*/
+console.warn('import can-event-queue/map/map instead.');
+module.exports = require('./map/map.js');
