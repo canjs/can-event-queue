f0524888a22f152678accc209dbab00bdf0735de
Chasen Le Hara <chasen@chasenlehara.com>
2018-07-03 22:19:33 -0700

    1.1.0

---
+++ dist/amd/can-event-queue.js
@@ -0,0 +1,11 @@
+/*can-event-queue@1.0.2#can-event-queue*/
+define([
+    'require',
+    'exports',
+    'module',
+    './map/map'
+], function (require, exports, module) {
+    'use strict';
+    console.warn('import can-event-queue/map/map instead.');
+    module.exports = require('./map/map');
+});
