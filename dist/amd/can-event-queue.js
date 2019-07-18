4ad393f51702cf64090a8741b7fc1ce9a42f2753
justinbmeyer <justinbmeyer@gmail.com>
2019-07-18 10:37:45 -0500

    1.1.7

---
+++ dist/amd/can-event-queue.js
@@ -0,0 +1,11 @@
+/*can-event-queue@1.1.6#can-event-queue*/
+define('can-event-queue', [
+    'require',
+    'exports',
+    'module',
+    'can-event-queue/map/map'
+], function (require, exports, module) {
+    'use strict';
+    console.warn('import can-event-queue/map/map instead.');
+    module.exports = require('can-event-queue/map/map');
+});
